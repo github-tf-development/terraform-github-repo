46fc5894cda18ccb17c7335fa52962feb296ce20
Ravi Kumar <RjDev@users.noreply.github.com>
2023-05-24 05:16:55 +0000

    addinng githubtoken

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "github" {}
+provider "github" {
+  token = var.GITHUB_ORG_TOKEN
+}
 
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "GITHUB_ORG_TOKEN" {
+
+}
+
+variable "GITHUB_ORG_NAME" {
+    
+}
